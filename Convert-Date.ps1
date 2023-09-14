0a6858a7a855c6d78618c3386f3487884601d9c1
Terence Luk <88775938+terenceluk@users.noreply.github.com>
2023-09-13 21:23:35 -0400

    Update Convert-Date.ps1

--- Convert-Date.ps1
+++ Convert-Date.ps1
@@ -6,6 +6,12 @@ Param
     )
 
 # Function that takes date and convert to yyyy-mm-dd
+# Works with:
+# Sat, May 27, 2023
+# Sat, May 27, 2023 1:55 PM GMT
+# Does not work with: Sat, May 27, 2023 1:55 PM GMT -0300
+# Use this Regex match [\s][-+]\d* ----> to match whitespace, match - or +, match a digit and 0 or more
+
 function ConvertDate {
     Param
     (
@@ -13,8 +19,8 @@ function ConvertDate {
         [string] $passedDate
     )
 
-    # Parse the input date string
-    $date = Get-Date $passedDate
+    # Parse the input date string (use RegEx expression to handle -0300 if passed)
+    $date = Get-Date ($passedDate -replace ('[\s][-+]\d*', '')) 
 
     # Format the date to yyyy-mm-dd
     $formattedDate = $date.ToString("yyyy-MM-dd")
